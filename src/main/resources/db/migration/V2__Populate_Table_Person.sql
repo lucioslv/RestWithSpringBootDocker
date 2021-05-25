98fc44689f597db732389bf18f6cb2f6866ab260
Lúcio Silva <lucio@ma.sesc.com.br>
2021-05-25 12:22:30 -0300

    Commit Inicial

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
+	(1, 'Lúcio', 'Silva', 'MA', 'Male'),
+	(6, 'Carla', 'Silva', 'MA', 'Female'),
+	(7, 'Julia', 'Silva', 'MA', 'Female')
